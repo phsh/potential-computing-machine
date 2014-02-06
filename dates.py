8e38f5d1e1daa069c3f100d52847622f1cce62ce
Per Hedman <per.hedman@unibet.com>
2014-02-06 14:27:48 +0100

    My initial personnumber generator

---
+++ dates.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import datetime
+
+def luhn_checksum(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d*2))
+    return checksum % 10
+
+
+def calculate_luhn(partial_card_number):
+    check_digit = luhn_checksum(int(partial_card_number) * 10)
+    return check_digit if check_digit == 0 else 10 - check_digit
+
+day = datetime.date(1900,1,1)
+while day < datetime.date(2015,1,1):
+	day = day + datetime.timedelta(days=1)
+	for i in range(1000):
+		number = day.strftime("%Y%m%d") + `i`.zfill(3)
+		print number + `calculate_luhn(int(number))`
