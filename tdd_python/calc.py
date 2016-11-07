4ec3fc86a87e8f484ec5d3202ba2ba8d692b050b
Per Hedman <phhedman@gmail.com>
2016-11-07 13:45:23 +0100

    debuging the broken tool

--- tdd_python/calc.py
+++ tdd_python/calc.py
@@ -8,7 +8,8 @@ lista = []
 for line in fileinput.input():
     lista.append(line.rstrip())
 result = "'" + str(lista[1]) + "' no such method supported. \n" + str(calcu.legal())
+print lista
 if lista[1] in calcu.legal():
 	result = getattr(calcu,list[1])(int(list[0]), int(list[2]) )
-print lista
+
 print result
