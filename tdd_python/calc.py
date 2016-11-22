00d8683d600a9a3b8336dbc4a24eb0e04788bb11
Per Hedman <phhedman@gmail.com>
2016-11-22 15:00:38 +0100

    Add code for clarification

--- tdd_python/calc.py
+++ tdd_python/calc.py
@@ -4,13 +4,15 @@ import fileinput
 from app.calculator import Calculator
 
 calcu = Calculator()
-lista = []
-for line in fileinput.input():
-	for lineParts in line.rstrip().split():
-		lista.append(lineParts)
-result = "'" + str(lista[1]) + "' no such method supported. \n" + str(calcu.legal())
-print lista
-if lista[1] in calcu.legal():
-	result = getattr(calcu,lista[1])(int(lista[0]), int(lista[2]) )	
-
+firstNumber = 0
+secondNumber = 2
+command = 1
+tripleCommand = []
+for compleatLine in fileinput.input():
+	for commandParts in compleatLine.rstrip().split():
+		tripleCommand.append(commandParts)
+result = "'" + str(tripleCommand[command]) + "' no such method supported. \n" + str(calcu.legal())
+print tripleCommand
+if tripleCommand[command] in calcu.legal():
+	result = getattr( calcu,tripleCommand[command])(int(tripleCommand[firstNumber]), int(tripleCommand[secondNumber]) )
 print result
