84d60b0c508cb3b79e56cd9779ef7be44e943e5f
Per Hedman <phhedman@gmail.com>
2016-11-22 15:01:35 +0100

    removed debug information

--- tdd_python/calc.py
+++ tdd_python/calc.py
@@ -12,7 +12,6 @@ for compleatLine in fileinput.input():
 	for commandParts in compleatLine.rstrip().split():
 		tripleCommand.append(commandParts)
 result = "'" + str(tripleCommand[command]) + "' no such method supported. \n" + str(calcu.legal())
-print tripleCommand
 if tripleCommand[command] in calcu.legal():
 	result = getattr( calcu,tripleCommand[command])(int(tripleCommand[firstNumber]), int(tripleCommand[secondNumber]) )
 print result
